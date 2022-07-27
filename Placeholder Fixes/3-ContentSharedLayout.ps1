6096093839b386046b3e78dfc3ace06db8a995c4
zell71 <dave.ivan.jones@gmail.com>
2022-07-27 16:49:10 +1000

    powershell scripts to fix placeholders
    upgrades placholders in sc8 to sc10

---
+++ Placeholder Fixes/3-ContentSharedLayout.ps1
@@ -0,0 +1,36 @@
+# 3-Script to Update the ID format in all the Contents for shared Layout-
+
+# specify your path here. It is most likely be page templates and page content
+$startPath = "/sitecore/content/bupacomau"
+$loopCounter = 0
+Get-ChildItem -Path $startPath -Language * -Version * -Recurse | ForEach-Object {
+    $item = $_;
+
+    Get-Rendering -Item $_ | ForEach-Object {
+        $rendering = $_;
+        $matches = [regex]::Matches($_.Placeholder, '(_[0-9a-fA-F]{8}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{12})')
+
+        if ($matches.Success) {
+            Write-Host "Match found in item - [$($item.Paths.FullPath)]"
+            Write-Host "Old Placeholder - [$($rendering.Placeholder)]"
+
+            $newPlaceholder = $rendering.Placeholder
+
+            $matches | ForEach-Object {
+                $renderingId = $_.Value
+                $newPlaceholder = $newPlaceholder.Replace($renderingId, "{$($renderingId.ToUpper())}-0")
+            }
+
+            $newPlaceholder = $newPlaceholder.Replace('{_', "-{")
+            Write-Host "New Placeholder - [$($newPlaceholder)]"
+
+            # comment following 2 lines if you don't want to do replacement, but want to have log of upcoming placeholder changes
+            $rendering.Placeholder = $newPlaceholder
+            Set-Rendering -Item $item -Instance $rendering
+        }
+        else {
+            # Write-Host "Record Not Matched for item number $loopCounter"
+        }
+        $loopCounter++
+    }
+}
---
+++ Placeholder Fixes/FixPublishedVersion-Single_Item.ps1
@@ -0,0 +1,70 @@
+# this script fixes placeholders in both Shared and Final Layout for the published item version.
+
+$startPath = "/sitecore/content/bupacomau/home"
+$ritems = @()
+Get-Item -Path $startPath | ForEach-Object {
+    $item = $_;
+    $renderings =  Get-Rendering -Item $item
+    # check if the item has renderings, i.e. not a datasource item
+    if($renderings) {
+        $webVersion = Get-Item -Path web: -ID $item.ID -ErrorAction SilentlyContinue
+        if ($webVersion) {
+          $minVersion = $webVersion.Version.Number - $count
+          $ritems += Get-Item -Path master: -ID $_.ID -Version * | Where-Object { $_.Version.Number -eq $minVersion }
+        }
+    }    
+}
+
+$ritems | ForEach-Object {
+    $vItem = $_;
+    $version = $_.Version.Number;
+    $sharedLayoutRenderings =  Get-Rendering -Item $_
+    $sharedLayoutRenderings | ForEach-Object {
+        $rendering = $_;
+        $matches = [regex]::Matches($_.Placeholder, '(_[0-9a-fA-F]{8}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{12})')
+
+        if ($matches.Success) {
+            Write-Host "Match found in Shared Layout for item - [$($vItem.Paths.FullPath)][$version]"
+            Write-Host "Old Placeholder - [$($rendering.Placeholder)]"
+
+            $newPlaceholder = $rendering.Placeholder
+
+            $matches | ForEach-Object {
+                $renderingId = $_.Value
+                $newPlaceholder = $newPlaceholder.Replace($renderingId, "{$($renderingId.ToUpper())}-0")
+            }
+
+            $newPlaceholder = $newPlaceholder.Replace('{_', "-{")
+            Write-Host "New Placeholder - [$($newPlaceholder)]"
+
+            # comment following 2 lines if you don't want to do replacement, but want to have log of upcoming placeholder changes
+            $rendering.Placeholder = $newPlaceholder
+            Set-Rendering -Item $vItem -Instance $rendering
+        }
+    }
+    
+    $finalLayoutRenderings =  Get-Rendering -Item $_ -FinalLayout
+    $finalLayoutRenderings | ForEach-Object {
+        $rendering = $_;
+        $matches = [regex]::Matches($_.Placeholder, '(_[0-9a-fA-F]{8}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{4}[-][0-9a-fA-F]{12})')
+
+        if ($matches.Success) {
+            Write-Host "Match found in Final Layout for item - [$($vItem.Paths.FullPath)][$version]"
+            Write-Host "Old Placeholder - [$($rendering.Placeholder)]"
+
+            $newPlaceholder = $rendering.Placeholder
+
+            $matches | ForEach-Object {
+                $renderingId = $_.Value
+                $newPlaceholder = $newPlaceholder.Replace($renderingId, "{$($renderingId.ToUpper())}-0")
+            }
+
+            $newPlaceholder = $newPlaceholder.Replace('{_', "-{")
+            Write-Host "New Placeholder - [$($newPlaceholder)]"
+
+            # comment following 2 lines if you don't want to do replacement, but want to have log of upcoming placeholder changes
+            $rendering.Placeholder = $newPlaceholder
+            Set-Rendering -Item $vItem -Instance $rendering -FinalLayout
+        }
+    }
+}
